7899fa0c2c617caa6885011f9f4926348875c7c8
LucasMarapodi <lucasmarapodi.dev@gmail.com>
2022-05-09 16:55:50 -0300

    first commit

---
+++ ProjetoPalindromo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoPalindromo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPalindromo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PalavraModel teste = new PalavraModel();
+            teste.PalavraInvertida = "";
+
+            return View(teste);
+        }
+        
+        [HttpPost]
+        public IActionResult index(PalavraModel palavraModel)
+        {
+            if (ModelState.IsValid){
+
+                try
+                {
+                    for (int i = palavraModel.Palavra.Length - 1; i >= 0; i--)
+                    {
+                        palavraModel.PalavraInvertida = palavraModel.PalavraInvertida + palavraModel.Palavra[i];
+                    }
+
+                    if (palavraModel.Palavra.ToLower() == palavraModel.PalavraInvertida.ToLower())
+                    {
+                        TempData["Mensagem"] = "É um palindromo!.";
+                    }
+                }
+                catch(Exception e)
+                {
+                    TempData["MensagemErro"] = "Erro : " + e.Message; 
+                }
+
+            }
+
+            return View(palavraModel);
+        }
+    }
+}
---
+++ ProjetoPalindromo/Models/PalavraModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoPalindromo.Models
+{
+    public class PalavraModel
+    {
+        [Required(ErrorMessage ="Informe uma palavra.")]
+        [MinLength(3,ErrorMessage = "No minimo {1} caracteres.")]
+        public string Palavra { get; set; }
+
+        public string PalavraInvertida { get; set; }
+
+        public string Palindromo { get; set; }
+    }
+}
